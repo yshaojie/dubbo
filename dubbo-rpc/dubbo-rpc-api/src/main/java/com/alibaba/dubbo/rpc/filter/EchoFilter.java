b4ba70ebc3b055f1de197eb1f2efef30ea847783
yshaojie <ueshaojie_2010@126.com>
2015-08-05 15:51:16 +0800

    up version 2.5.5

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/EchoFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/EchoFilter.java
@@ -30,7 +30,7 @@ import com.alibaba.dubbo.rpc.RpcResult;
  * @author william.liangf
  */
 @Activate(group = Constants.PROVIDER, order = -110000)
-public class EchoFilter implements Filter {
+public class  EchoFilter implements Filter {
 
 	public Result invoke(Invoker<?> invoker, Invocation inv) throws RpcException {
 		if(inv.getMethodName().equals(Constants.$ECHO) && inv.getArguments() != null && inv.getArguments().length == 1 )
